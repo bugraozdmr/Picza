4c95c6afc412970ceea7de87fb3337fee29120b1
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2024-12-10 11:27:55 +0300

    fix

---
+++ src/main/java/com/grant/Picza/PiczaApplication.java
@@ -0,0 +1,13 @@
+package com.grant.Picza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiczaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiczaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grant/Picza/PiczaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grant.Picza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiczaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
